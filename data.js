8fdb32ff73969191ab4c02c9657ba173ce7b2773
Oluwaphemmy <ojokofemi@gmail.com>
2022-07-14 10:00:09 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,256 @@
+export const QUESTIONS = [
+  {
+    // Question 1
+    question: `What was Sarah's reaction to the news that she would bear a son?`,
+    answers: [`She laughed`, `She cried`, `She rejoiced`, `She fainted`],
+    correctAnswer: `She laughed`,
+    bibleText: `Genesis 18:12-13`,
+  },
+  {
+    // Question 2
+    question: `Who was NOT one of the three Hebrew boys cast into the fiery furnace?`,
+    answers: [`Shadrach`, `Meshach`, `Abednego`, `Daniel`],
+    correctAnswer: `Daniel`,
+    bibleText: `Daniel 3:20`,
+  },
+  {
+    // Question 3
+    question: `Who was David's best friend?`,
+    answers: [`Jonathan`, `Saul`, `Samuel`, `Goliath`],
+    correctAnswer: `Jonathan`,
+    bibleText: `1 Samuel 20:42`,
+  },
+  {
+    // Question 4
+    question: `Who is NOT currently in heaven with God?`,
+    answers: [`Enoch`, `Elijah`, `Moses`, `Solomon`],
+    correctAnswer: `Solomon`,
+    bibleText: `Genesis 5:23-24; Matthew 17:3`,
+  },
+  {
+    // Question 5
+    question: `Who was cast into a den of lions?`,
+    answers: [`Daniel`, `Joseph`, `Elisha`, `Job`],
+    correctAnswer: `Daniel`,
+    bibleText: `Daniel 6:16`,
+  },
+  {
+    // Question 6
+    question: `Where was Jesus mostly raised as a child?`,
+    answers: [`Jerusalem`, `Bethlehem`, `Nazareth`, `Egypt`],
+    correctAnswer: `Nazareth`,
+    bibleText: `Matthew 2:23`,
+  },
+  {
+    // Question 7
+    question: `What was the 12-year-old Jesus doing when His parents found Him?`,
+    answers: [
+      `Praying to His Father`,
+      `Fishing with nets`,
+      `Teaching in the temple`,
+      `Playing with other kids`,
+    ],
+    correctAnswer: `Teaching in the temple`,
+    bibleText: `Luke 2:46-47`,
+  },
+  {
+    // Question 8
+    question: `What happened immediately after Peter denied Christ three times?`,
+    answers: [
+      `Peter fell dead`,
+      `The ground shook`,
+      `Peter went blind`,
+      `A rooster crowed`,
+    ],
+    correctAnswer: `A rooster crowed`,
+    bibleText: `Matthew 26:74`,
+  },
+  {
+    // Question 9
+    question: `At the moment Jesus died on the cross, which of the following things did NOT happen?`,
+    answers: [
+      `Earthquake`,
+      `Jesus came to life`,
+      `Dead came to life`,
+      `Temple veil was torn`,
+    ],
+    correctAnswer: `Jesus came to life`,
+    bibleText: `Matthew 27:51-53`,
+  },
+  {
+    // Question 10
+    question: `What animal attacked Paul after being shipwrecked on the island of Malta?`,
+    answers: [`Snake`, `Bear`, `Mule`, `Scorpion`],
+    correctAnswer: `Snake`,
+    bibleText: `Acts 28:3`,
+  },
+  {
+    // Question 11
+    question: `Who were the first disciples called by Jesus`,
+    answers: [
+      `Peter and John`,
+      `Peter and Andrew`,
+      `Phillip and John`,
+      `Andrew and John`,
+    ],
+    correctAnswer: `Peter and Andrew`,
+    bibleText: `Matthew 4:18-22`,
+  },
+  {
+    // Question 12
+    question: `Paul was shipwrecked on what island`,
+    answers: [`Malta`, `Patmos`, `Camel`, `Greece`],
+    correctAnswer: `Malta`,
+    bibleText: `Act 27:1-28`,
+  },
+  {
+    // Question 13
+    question: `Who recognized Jesus as messiah when he was presented to the temple as a baby?`,
+    answers: [`Wise men`, `John the baptist`, `Anna`, `Simeon`],
+    correctAnswer: `Simeon`,
+    bibleText: `Luke 2 :22-23`,
+  },
+  {
+    // Question 14
+    question: `Who is the high priest  of Jerusalem that put Jesus on trial?`,
+    answers: [`Agrippa`, `Pilate`, `Cephas`, `Caiaphas`],
+    correctAnswer: `Caiaphas`,
+    bibleText: `Matt 25 : 56-67`,
+  },
+  {
+    // Question 13
+    question: `Which disciples doubts jesus' ressurrection untill he see jesus with his own eyes?`,
+    answers: [`James`, `Thomas`, `Philip`, `Peter`],
+    correctAnswer: `Thomas`,
+    bibleText: `John 20:24-29`,
+  },
+  {
+    // Question 14
+    question: `How many people entered Noah's ark?`,
+    answers: [`12`, `10`, `8`, `9`],
+    correctAnswer: `8`,
+    bibleText: `Gen 7:13`,
+  },
+  {
+    // Question 15
+    question: `Paul was shipwrecked on what island?`,
+    answers: [`Malta`, `Patmos`, `Camel`, `Greece`],
+    correctAnswer: `Malta`,
+    bibleText: `Act 27:1-28`,
+  },
+  {
+    // Question 16
+    question: `What did God create on the fourth day?`,
+    answers: [`Firmament`, `Trees`, `Sun, moon and stars`, `Animals`],
+    correctAnswer: `Sun, moon and stars`,
+    bibleText: `Gen 1 : 14-16`,
+  },
+  {
+    // Question 17
+    question: `Who is the first murderer in the bible?`,
+    answers: [`Seth`, `Adam`, `Abel`, `Cain`],
+    correctAnswer: `Cain`,
+    bibleText: `Act 27:1-28`,
+  },
+  {
+    // Question 18
+    question: `Which place is called 'Zion' and ' the city of David'?`,
+    answers: [`Cannaan`, `Rome`, `Jerusalem`, `Nazareth`],
+    correctAnswer: `Jerusalem`,
+    bibleText: `2 samuel 5:9`,
+  },
+  {
+    // Question 19
+    question: `Which disciple walked on water?`,
+    answers: [`Peter`, `James`, `John`, `Philip`],
+    correctAnswer: `Peter`,
+    bibleText: `Matt 14 : 22-31`,
+  },
+  {
+    // Question 20
+    question: `How many days did it take God to create the world?`,
+    answers: [`S6`, `9`, `7`, `8`],
+    correctAnswer: `6`,
+    bibleText: `Gen 2:2`,
+  },
+  {
+    // Question 21
+    question: `Where did god give moses the ten commandment?`,
+    answers: [`Mount Patmos`, `Mount Harat`, `Mount Camel`, `mount Sinai`],
+    correctAnswer: `Mount Sinai`,
+    bibleText: `Exodus 19: 1-40`,
+  },
+  {
+    // Question 22
+    question: `What golden image did the isrealites make mount Sinai?`,
+    answers: [
+      `A golden Calf`,
+      `A golden sheep`,
+      `A golden sheep`,
+      `A golden Cow`,
+    ],
+    correctAnswer: `A golden calf`,
+    bibleText: `Exodus 32`,
+  },
+  {
+    // Question 23
+    question: `Who was the woman judge who led isreal to victory?`,
+    answers: [`Abigail`, `Nariam`, `Anna`, `Deborah`],
+    correctAnswer: `Deborah`,
+    bibleText: `Judges 4`,
+  },
+  {
+    // Question 24
+    question: `With what did samson killed 1,000 Philistines?`,
+    answers: [
+      `A Sword`,
+      `A lion jawbone`,
+      `A donkey jawbone`,
+      `A  cow jawbone`,
+    ],
+    correctAnswer: `A donkey jawbone`,
+    bibleText: `Judges 15: 19`,
+  },
+  {
+    // Question 25
+    question: `When David wanted to fight goliath, what did saul try to give him?`,
+    answers: [`Sling`, `Shield`, `Armour`, `Sword`],
+    correctAnswer: `Armour`,
+    bibleText: `! samuel 17`,
+  },
+  {
+    // Question 26
+    question: `What did david take from the brook to fight Goliagth?`,
+    answers: [`6 stones`, `5 stones`, `4 stones`, `3 stones`],
+    correctAnswer: `5 stones`,
+    bibleText: ` 1 samuel 17: 26-50`,
+  },
+  {
+    // Question 27
+    question: `What country did David run away to when Saul wanted to kill him?`,
+    answers: [`Gibeon`, `Babylon`, `Philistines`, `Canaan`],
+    correctAnswer: `Philistines`,
+    bibleText: `1 samuel 21 :10`,
+  },
+  {
+    // Question 28
+    question: `How many times did David spared Saul's life?`,
+    answers: [`4 times`, `once`, `thrice`, `twice`],
+    correctAnswer: `twice`,
+    bibleText: `1 samuel 24 and 26`,
+  },
+  {
+    // Question 29
+    question: `Who was the captain of David army?`,
+    answers: [`Jael`, `Ahitophel`, `Abinadab`, `Joab`],
+    correctAnswer: `Joab`,
+    bibleText: `2 samuel 24 : 2`,
+  },
+  {
+    // Question 30
+    question: `Which woman did David commit adultery with?`,
+    answers: [`Ahinoam`, `Mariam`, `Bethsheba`, `Abigail`],
+    correctAnswer: `Bethsheba`,
+    bibleText: ` 2 samuel 11`,
+  },
+];
---
+++ script.js
@@ -0,0 +1,283 @@
+"use strict";
+
+import { QUESTIONS } from "./data.js";
+
+// Universal variables to keep track of quiz state
+let currentQuesNum = 0;
+let score = 0;
+
+// Simple utility to control whether console log will display
+const LOG_ON = true;
+const logToggle = (msg) => {
+  if (LOG_ON) {
+    console.log(msg);
+  }
+};
+
+// shuffles the items in an array
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+    [array[i], array[j]] = [array[j], array[i]]; // swap elements
+  }
+}
+
+// Update the DOM to display the header progress items
+function renderProgress() {
+  logToggle("renderProgress() ran");
+  $("#js-progressNum").text(`# ${currentQuesNum + 1}`);
+  //$("#js-progressScore").text(`Score: ${score}/10`);
+}
+
+// Update the DOM to display the home screen of the quiz
+function renderQuizHome() {
+  logToggle("renderQuizHome() ran");
+  const homeStartUpString = `<h2 id="intro">How well do you know the Bible?</h2>
+        <button id="js-startQuiz">Begin your Quest</button>`;
+  $("#js-quizInfo").html(homeStartUpString);
+  $("#js-questionForm").html("");
+}
+
+// Generate the HTML for a single answer
+function generateAnswerElement(answer, index) {
+  return `<input type="radio" tabindex="-1" name="answers" id="answer${index}" value="${answer}">
+            <label class="js-answer answerLabel" tabindex="0" role="button" aria-pressed="false" for="answer${index}">${answer}</label>`;
+}
+
+// Generate the HTML string for all answers
+function generateAnswerString(answers) {
+  logToggle("Generating answer elements");
+  const answerHTML = answers.map((ans, index) =>
+    generateAnswerElement(ans, index)
+  );
+  return answerHTML.join("");
+}
+
+// Update the DOM to display the current question
+function renderQuestion() {
+  logToggle("renderQuestion() ran");
+  const currentQuestion = QUESTIONS[currentQuesNum];
+  shuffle(currentQuestion.answers);
+
+  // Create HTML for Question Form
+  const questionHTMLString = `<fieldset id="questionAnswerSet">
+            <legend>${currentQuestion.question}</legend>
+            ${generateAnswerString(currentQuestion.answers)}
+            <button id="js-submitAnswer">Show Answer</button>
+        </fieldset>`;
+
+  // Remove current info section
+  $("#js-quizInfo").html("");
+  // Add question form
+  $("#js-questionForm").html(questionHTMLString);
+  renderProgress();
+}
+
+// Event handler for the Start Button
+function handleStartButton() {
+  $("#js-quizInfo").on("click", "#js-startQuiz", (event) => {
+    logToggle("handleStartButton() ran");
+    shuffle(QUESTIONS);
+    renderQuestion();
+    // Change background for questions
+    $("body").css(
+      "background-image",
+      "url(images/aaron-burden-320238-unsplash.jpg)"
+    );
+  });
+}
+
+// Update the DOM to remove the header progress items
+function clearProgress() {
+  logToggle("clearProgress() ran");
+  $("#js-progressNum").text("");
+  $("#js-progressScore").text("");
+}
+
+// Generate the HTML for result
+function generateQuestionResultString(isCorrect) {
+  return `<div id="result" role="none">
+                <h2>${
+                  isCorrect
+                    ? "Hallelujah!"
+                    : "Looks like you need more studying!"
+                }</h2>
+                ${
+                  isCorrect
+                    ? ""
+                    : `<p>The correct answer is: 
+                    <em id="answerText">${QUESTIONS[currentQuesNum].correctAnswer}.</em>
+                </p>`
+                }
+                <p>You can read this story in ${
+                  QUESTIONS[currentQuesNum].bibleText
+                }.</p>
+            </div>
+            <button id="js-next" >Continue your Quest</button>`;
+}
+
+// Update the DOM to display the result of answering a question
+function renderQuestionResult(isCorrect) {
+  logToggle("renderQuestionResult() ran");
+
+  // Add results to information section
+  $("#js-quizInfo").html(generateQuestionResultString(isCorrect));
+
+  // Remove question
+  $("#js-questionForm").html("");
+  renderProgress();
+
+  // Bible Gateway JS Utility to convert Bible references into URL links
+  BGLinks.version = "NLT";
+  BGLinks.linkVerses();
+}
+
+// Determines whether user answered question correctly
+function determineQuestionResult() {
+  const userAnswer = $("input[type='radio']:checked").val();
+  let userIsCorrect = QUESTIONS[currentQuesNum].correctAnswer === userAnswer;
+  score += userIsCorrect ? 1 : 0;
+  renderQuestionResult(userIsCorrect);
+}
+
+// Handle Key commands for accessibility
+// Customization is necessary since radio button is not navigable.
+function handleQuestionSelect() {
+  $(document).keydown((event) => {
+    const keycode = event.key ? event.key : event.which;
+    const focusedLabel = $(".js-answer:focus");
+    const currentIndex = $(".js-answer").index(focusedLabel);
+    const prevInput = focusedLabel.prev("input[type='radio']");
+    if (prevInput.val() != null) {
+      switch (keycode) {
+        case "Enter":
+        case " ":
+          // Select the current answer
+          prevInput.prop("checked", true);
+          break;
+        case "ArrowUp":
+        case "ArrowLeft":
+          // Navigate to previous item
+          $(".js-answer")
+            .eq((currentIndex - 1) % 4)
+            .focus();
+          break;
+        case "ArrowDown":
+        case "ArrowRight":
+          // Navigate to next item
+          $(".js-answer")
+            .eq((currentIndex + 1) % 4)
+            .focus();
+          break;
+        default:
+      }
+    }
+  });
+}
+
+// Event handler for validating the Question form
+function handleValidateForm() {
+  $("#js-questionForm").validate({
+    submitHandler: (form, event) => {
+      event.preventDefault();
+      determineQuestionResult();
+    },
+    rules: {
+      answers: "required",
+    },
+    messages: {
+      answers: {
+        required: "<i class='fas fa-dove'></i> Please select a response!",
+      },
+    },
+    errorPlacement: (error, element) => {
+      if (element.is(":radio")) {
+        error.prependTo(element.parents("#questionAnswerSet"));
+      } else {
+        // This is the default behavior
+        error.insertAfter(element);
+      }
+    },
+    onclick: false,
+  });
+}
+
+// Generate the HTML for final quiz results
+function generateQuizResultString(isWinner) {
+  return `<div id="result">
+                <h2>${
+                  isWinner
+                    ? "The Wisdom of God is in you!"
+                    : "Do not be discouraged!<br>God is with you wherever you may go."
+                }
+                </h2>
+                <p id="finalScore">You scored ${score}/10!</p>
+                ${
+                  isWinner
+                    ? `<h3>"Therefore, go and make disciples of all the nations!"</h3>
+                        <p>Matthew 28:19</p>`
+                    : `<h3>"Let the message about Christ, in all its richness, fill your life!"</h3>
+                        <p>Colossians 3:16</p>`
+                }
+            </div>
+            <button id="js-resetQuiz">Restart your Quest</button>`;
+}
+
+// Update the DOM to display the final result of the quiz
+function renderQuizResult(isWinner) {
+  logToggle("renderQuizResult() ran");
+
+  // Add results to information section
+  $("#js-quizInfo").html(generateQuizResultString(isWinner));
+
+  // Remove question
+  $("#js-questionForm").html("");
+  clearProgress();
+
+  // Bible Gateway JS Utility to convert Bible references into URL links
+  BGLinks.version = "NLT";
+  BGLinks.linkVerses();
+}
+
+// Event handler for the Next Button
+function handleNextButton() {
+  $("#js-quizInfo").on("click", "#js-next", (event) => {
+    logToggle("handleNextButton() ran");
+    currentQuesNum += 1;
+    if (currentQuesNum < 100) {
+      renderQuestion();
+    } else {
+      renderQuizResult(score > 5);
+    }
+  });
+}
+
+// Event handler for the Reset Button
+function handleResetButton() {
+  $("#js-quizInfo").on("click", "#js-resetQuiz", (event) => {
+    logToggle("handleResetButton() ran");
+    currentQuesNum = 0;
+    score = 0;
+    renderQuizHome();
+    // Change Background for Home screen
+    $("body").css(
+      "background-image",
+      "url(images/aaron-burden-759770-unsplash.jpg)"
+    );
+  });
+}
+
+// Starts the Event handlers used to run the Quiz
+function handleQuiz() {
+  logToggle("handleQuiz() ran");
+
+  renderQuizHome();
+  handleStartButton();
+  handleQuestionSelect();
+  handleValidateForm();
+  handleNextButton();
+  handleResetButton();
+}
+
+// Start the quiz app
+$(handleQuiz);
